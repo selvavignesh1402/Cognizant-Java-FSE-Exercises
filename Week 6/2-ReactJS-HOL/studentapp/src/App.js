29e7714f4e489939a2fc87583ce701d41917e8a4
selvavignesh1402 <selvavigneshvijayan1402@gmail.com>
2025-07-27 18:10:51 +0530

    Week 6 Task-2

---
+++ Week 6/2-ReactJS-HOL/studentapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './Component/Home';
+import About from './Component/About';
+import Contact from './Component/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <About/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
